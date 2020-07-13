2976b27b1f4efa3e3a12113ff7ac41a0edf68783
Paulo Castellano <paulofcastellano@gmail.com>
2020-07-13 17:53:36 -0300

    Small update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ChangelogfyModule } from 'changelogfy-angular'; // <-- import the RatifyModule from ratify library we built
+import { ChangelogfyModule } from 'changelogfy-angular';
 
 @NgModule({
   declarations: [AppComponent],
